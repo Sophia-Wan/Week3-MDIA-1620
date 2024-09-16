37486ee3642d7099f041834837bc09ac1bc25a4c
Sophia <wwan18@my.bcit.ca>
2024-09-16 09:22:56 -0700

    second

---
+++ test.js
@@ -0,0 +1,7 @@
+const rainbow = "sky"
+console.log(
+"A"+rainbow+ "is a stunning natural phenomenon that appears when sunlight is refracted, dispersed, and reflected in water droplets, creating a spectrum of colors in the sky. This vibrant arc typically displays a sequence of red, orange, yellow, green, blue, indigo, and violet, offering a brief but magical glimpse of nature's artistry." + rainbow + "often follow a rain shower, serving as a colorful reminder of the sun's return and the beauty that can emerge after a storm.");
+
+
+console.log('i\'m happy');
+
